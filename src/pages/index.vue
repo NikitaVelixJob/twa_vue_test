403261ff8cf1ff9f6e48d3db39b0466c776349c3
NikitaVelixJob <nikitavelix@yahoo.com>
2024-01-22 23:36:57 +0600

    haptic test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,10 +4,23 @@ console.log(window.Telegram)
 
 function toggleMainButton() {
   const isVisible = window.Telegram.WebApp.MainButton.isVisible
-  if (isVisible)
+  if (isVisible) {
     window.Telegram.WebApp.MainButton.hide()
-  else
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('hard')
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('hard')
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('hard')
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('hard')
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('hard')
+  }
+  else {
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('light')
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('light')
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('light')
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('light')
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('light')
+    window.Telegram.WebApp.HapticFeedback.impactOccurred('light')
     window.Telegram.WebApp.MainButton.show()
+  }
 }
 </script>
 
@@ -22,35 +35,35 @@ function toggleMainButton() {
 </template>
 
 <style>
-body{
-	color: var(--tg-theme-text-color);
-	background: var(--tg-theme-bg-color);
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	font-size: 18px;
+body {
+  color: var(--tg-theme-text-color);
+  background: var(--tg-theme-bg-color);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 18px;
 }
 
-.hint{
-	color: var(--tg-theme-hint-color);
+.hint {
+  color: var(--tg-theme-hint-color);
 }
 
-.link{
-	color: var(--tg-theme-link-color);
+.link {
+  color: var(--tg-theme-link-color);
 }
 
-.button{
-	background: var(--tg-theme-button-color);
-	color: var(--tg-theme-button-text-color);
-	border: none;
-	font-size: 18px;
+.button {
+  background: var(--tg-theme-button-color);
+  color: var(--tg-theme-button-text-color);
+  border: none;
+  font-size: 18px;
 }
 
-.button:not(:last-child){
-	margin-bottom: 20px
+.button:not(:last-child) {
+  margin-bottom: 20px
 }
 
-#usercard{
-	text-align: center;
+#usercard {
+  text-align: center;
 }
 </style>
