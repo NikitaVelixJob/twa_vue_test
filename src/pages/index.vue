c3a6195bb0b7836e982fd14f1ea2f8d28dca850a
NikitaVelixJob <nikitavelix@yahoo.com>
2024-01-22 23:32:50 +0600

    first

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,56 @@
 <script setup lang="ts">
 const { t } = useI18n()
+console.log(window.Telegram)
+
+function toggleMainButton() {
+  const isVisible = window.Telegram.WebApp.MainButton.isVisible
+  if (isVisible)
+    window.Telegram.WebApp.MainButton.hide()
+  else
+    window.Telegram.WebApp.MainButton.show()
+}
 </script>
 
 <template>
-  <div>
-    <h1>{{ t('hello') }}</h1>
+  <div class="w-screen h-100dvh">
+    hi its a test of webapp
+
+    <button class="bg-" @click="toggleMainButton">
+      TOGGLE MAIN BUTTOn
+    </button>
   </div>
 </template>
 
 <style>
+body{
+	color: var(--tg-theme-text-color);
+	background: var(--tg-theme-bg-color);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	font-size: 18px;
+}
+
+.hint{
+	color: var(--tg-theme-hint-color);
+}
+
+.link{
+	color: var(--tg-theme-link-color);
+}
+
+.button{
+	background: var(--tg-theme-button-color);
+	color: var(--tg-theme-button-text-color);
+	border: none;
+	font-size: 18px;
+}
+
+.button:not(:last-child){
+	margin-bottom: 20px
+}
+
+#usercard{
+	text-align: center;
+}
 </style>
