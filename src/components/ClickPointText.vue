326c9c3ff89e5f6320e2eb65dc52f5f6551c07f7
NikitaVelixJob <nikitavelix@yahoo.com>
2024-01-23 19:32:36 +0600

    feat: points for click

---
+++ src/components/ClickPointText.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+defineProps<{
+  amount: string
+  color: string
+}
+>()
+
+onActivated(() => {
+  window.Telegram.WebApp.HapticFeedback.impactOccurred('light')
+})
+</script>
+
+<template>
+  <div class="w-full h-full fixed flex items-center justify-center z-0 got_coin" :class="color">
+    <div class="font-black text-8xl">
+      {{ amount }}
+    </div>
+  </div>
+</template>
+
+<style>
+.got_coin {
+    animation: coinAnimation 1.5s ease-in-out ;
+}
+
+@keyframes coinAnimation {
+    0% {
+        opacity: 0;
+    }
+    1%{
+        opacity: 100;
+    }
+    65% {
+        opacity: 75;
+    }
+    100% {
+        translate: 0 -50%;
+        opacity: 0
+    }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,40 +1,78 @@
 <script setup lang="ts">
-const { t } = useI18n()
-console.log(window.Telegram)
+import type { Component } from 'vue'
+import { v4 as uuidv4 } from 'uuid'
+import ClickPointText from '../components/ClickPointText.vue'
 
-function toggleMainButton() {
-  const isVisible = window.Telegram.WebApp.MainButton.isVisible
-  if (isVisible) {
-    window.Telegram.WebApp.MainButton.hide()
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('heavy')
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('heavy')
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('heavy')
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('heavy')
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('heavy')
+const { t } = useI18n()
+
+const textComponents = ref<{
+  id: any
+  amount: string
+  color: string
+  component: Component
+}[]>([])
+const coins = ref()
+function onClick() {
+  // generate a new insanse of text on click
+  const text = '12'
+  const textColor = 'text-orange-400'
+
+  const item = {
+    id: uuidv4(),
+    amount: text,
+    color: textColor,
+    component: ClickPointText,
   }
-  else {
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('soft')
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('soft')
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('soft')
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('soft')
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('soft')
-    window.Telegram.WebApp.HapticFeedback.impactOccurred('soft')
-    window.Telegram.WebApp.MainButton.show()
-  }
+  textComponents.value.push(item)
+
+  // remove item from array in 1.5s
+  setTimeout(() => {
+    textComponents.value = textComponents.value.filter(i => i.id !== item.id)
+    coins.value.children[0].remove()
+  }, 1500)
 }
 </script>
 
 <template>
-  <div class="w-screen h-100dvh">
-    hi its a test of webapp
+  <div
+    :style="{ backgroundImage: `url(/attr/bg/DarkForest.png)`, backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundSize: 'cover' }"
+    class="w-screen h-100dvh relative object-cover object-bottom "
+  >
+    <!-- <ClickPointText amount="10" color="text-orange-400" /> -->
+    <div ref="coins" class="w-full h-full absolute">
+      <template v-for="item in textComponents" :key="item.id">
+        <component
+          :is="item.component"
+          :id="item.id"
+          :amount="item.amount"
+          :color="item.color"
+        />
+      </template>
+    </div>
 
-    <button class="bg-" @click="toggleMainButton">
-      TOGGLE MAIN BUTTOn
-    </button>
+    <div class="h-full w-full flex items-end">
+      <div class="w-full h-full flex items-end justify-center group z-10 max-h-70% mt-auto" @click="onClick">
+        <img src="/buns/Police.png" alt="bun" class="group-active:scale-90 scale-100 h-full mt-auto">
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
+:root {
+  --tg-theme-bg-color: #0a0a0a;
+  --tg-theme-text-color: #fff;
+  --tg-theme-hint-color: #929292;
+  --tg-theme-link-color: #207ae4;
+  --tg-theme-button-color: #5bc8fb;
+  --tg-theme-button-text-color: #fffeec;
+  --tg-theme-secondary-bg-color: #f3f2f9;
+
+  --default-font: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+    'Segoe UI Symbol';
+}
+
 body {
   color: var(--tg-theme-text-color);
   background: var(--tg-theme-bg-color);
